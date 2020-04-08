d55f4cf47d479cd4ca9ccdf36ec7edf29e00aaf9
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-04-08 07:23:52 -0700

    I think MVP.

--- src/hashtable.py
+++ src/hashtable.py
@@ -10,11 +10,11 @@ class LinkedPair: #(Node)
     def __repr__(self):
         return f"<{self.key}, {self.value}, {self.next}>"
 
-    # Returns a tuple as a string
-    def __str__(self):
-        key = self.key
-        value = self.value
-        return "A tuple of {}, {}".format(key, value)
+    # # Returns a tuple as a string
+    # def __str__(self):
+    #     key = self.key
+    #     value = self.value
+    #     return "{}, {}".format(key, value)
 
 
 class HashTable:
@@ -67,63 +67,26 @@ class HashTable:
         # 1. Compute index of key
         index = self._hash_mod(key)
         # print(index)
-        # Go to the node corresponding to the hash
-        node = self.storage[index]
+        # 2. Go to the current index (node) with that hashed index
+        current_index = self.storage[index]
 
-        # 3. If bucket is empty:
-        if node is None:
-            # Create node, add it, return
+        # 3. If current index (node) is empty:
+        if current_index is None:
+            # Create current index (node), add it, return
             self.storage[index] = LinkedPair(key, value)
             
             return
 
         # 4. Collision! Iterate to the end of the linked list at provided index
-        prev = node
-        while node is not None:
-            prev = node
-            node = node.next
-            print("Collision occured: node already has the same key ")
-        # Add a new node at the end of the list with provided key/value
-        prev.next = LinkedPair(key, value)
+        prev_index = current_index
+        while current_index is not None:
+            prev_index = current_index
+            current_index = current_index.next
+            print(f"Collision occured: {prev_index} already has the same key ")
+        # Add a new previous index (node) at the end of the list with provided key/value
+        prev_index.next = LinkedPair(key, value)
 
 
-
-        # # Checks for index of a key
-        # index = self._hash_mod(key)
-        # # Current value of the hash (node)
-        # current_value = self.storage[index]
-        # print(index)
-        # # If a node already has the same key
-        # if current_value is not None:
-        #     # print("Collision occured: node already has the same key ")
-
-        #     # Create a new kvp from the LinkedPair class
-        #     # newLinkedPair = LinkedPair(key, value)
-        #     prev_value = None
-        #     while current_value is not None:
-        #         if prev_value.key == key:
-        #             current_value.value = value
-        #             return
-        #         prev_value = current_value
-        #         current_value = current_value.next
-        #     current_value.next = LinkedPair(key, value)
-        # else:
-        #     self.storage[index] = LinkedPair(key, value)
-
-            # while current_value is not None:
-            #     prev_value = current_value
-            #     current_value = current_value.next
-            # # Adds new node to the end
-            # newLinkedPair.next = self.storage[index]
-            # self.storage[index] = newLinkedPair
-        # If node doesnt't already has the same key and exist make new and add
-        # else:
-        #     self.storage[index] = LinkedPair(key, value)
-        
-       
-      
-
-    
     def remove(self, key):
         '''
         Remove the value stored with the given key.
@@ -142,41 +105,7 @@ class HashTable:
             # Print warning one isn't found
             print("Warning at remove: Key does not exist.")
 
-        # # Check the index
-        # if self.storage[index] is not None:
-        #     head = self.storage[index]
-        #     if head.next is None:
-        #         # Since at the head, can just remove it
-        #         self.storage[index] = None
-        #     else:
-        #         # Traverse the LinkedPair
-        #         if head.key == key:
-        #             self.storage[index] = head.next
-        #         else:
-        #             prev = head
-        #             current = prev.next
-                    
-        #             while current is not None:
-        #                 # Check for the right key
-        #                 if key == current.key:
-        #                     # If it's the right key, replace with None
-        #                     prev.next = current.next
-        #                 current = current.next
-            
-        # # If doesn't exist, return None
-        # else:
-        #     return None
-       
-        # # Find the index
-        # index = self._hash_mod(key) #<- puts into the index
-        # # Checks if there is a value store at the index
-        # if self.storage[index] is not None:
-        #     # Sets it to NONE
-        #     self.storage[index] = None
-        # else:
-        #     print("Warning: Key is not found.")
-
-
+        
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
@@ -185,28 +114,24 @@ class HashTable:
 
         Fill this in.
         '''
+        # Finds the index using the hash function
         index = self._hash_mod(key)
-        value_item = self.storage[index]
-        print(index)
-        if value_item is None:
-            return None 
+        # Goes to the first slot with that hashed index
+        current_index = self.storage[index]
+        # print(index)
 
-        while value_item:
-            if value_item.key is not key:
-                value_item = value_item.next 
+        # if current_index is None:
+        #     return None 
+        # Goes through the LinkedPair at this hashed index (node)
+        while current_index:
+            # Current index is now either requested kvp OR Nonw
+            if current_index.key is not key:
+                current_index = current_index.next 
             else:
-                return value_item.value 
+                return current_index.value 
         return None
 
-        # # Check if a pair exists with matching keys
-        # if self.storage[index] is not None and self.storage[index].key == key:
-        #     return self.storage[index].value
-        # else:
-        #     # Return None
-        #     return None
        
-
-
     def resize(self):
         '''
         Doubles the capacity of the hash table and
